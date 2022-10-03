73fcd0289c7fe7d835ae88078bf61f9bd8c54d30
emmanuel_ojogwu@yahoo.com <98138641+KingNuel1@users.noreply.github.com>
2022-10-03 17:04:43 -0500

    update 1.7

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -67,9 +67,9 @@ const Experience = () => {
         }    
     ]
   return (
-    <div name='experience' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen' >
-        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
-            <div className='p-4'>
+    <div name='experience' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen' >
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-white'>
+            <div>
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
                 <p className='py-6'>These are some of the technologies I've worked with</p>
             </div>
